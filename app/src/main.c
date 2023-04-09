80a5a81713d0d702a658acc2b73914577a6e9bff
David Logr <logrik@email.cz>
2023-04-10 00:35:48 +0200

    funkcni tlacitka, priprava na LCD

--- app/src/main.c
+++ app/src/main.c
@@ -1,6 +1,7 @@
 #include "stm8s.h"
 // #include "encoder.h"
 #include "milis.h"
+#include "LCD_I2C.h"
 
 #define RGB_PORT    GPIOD
 #define RED_PIN     GPIO_PIN_4
@@ -16,50 +17,77 @@
 #define micros_init milis_init
 #define micros      milis
 
+void colour_control(void);
 void counter_reset(void);
 
 uint32_t master_time            = 0;
 uint8_t  master_period          = 1; 
 uint8_t  counter[4]             = {0, 0, 0, 0};         // master, R, G, B
-uint8_t  counter_reference[4]   = {0, 100, 10, 10};      // master, R, G, B
-uint8_t  index                  = 0;
+int8_t   counter_reference[4]   = {0, 0, 0, 0};      // master, R, G, B
+uint8_t  index                  = 1;
 
 // INTERRUPT_HANDLER(EXTI_PORTA_IRQHandler, 3)
 // {
-//     switch (GPIO_ReadInputData(CTRL_PORT))
+//     if (GPIO_ReadInputPin(CTRL_PORT, CHANGE) == RESET)
 //     {
-
-//     case 0b010:
-//         if ((index + 1) > 2)
+//         index += 1;
+//         if (index > 3)
 //         {
-//             index = 0;
-//         } else {
-//             index += 1;
+//             index = 1;
 //         }
-//         break;    
+//     }
 
-//     case 0b001:
-//         if ((counter_reference[index] - 1) < 0)
-//         {
-//             counter_reference[index] = 0;
-//         } else {
-//             counter_reference[index] -= 1;
-//         }
-//         break;
-
-//     case 0b100:
-//         if ((counter_reference[index] + 1) > 100)
+//     if (GPIO_ReadInputPin(CTRL_PORT, PLUS) == RESET)
+//     {
+//         counter_reference[index] += 1;
+//         if (counter_reference[index] >= 100)
 //         {
 //             counter_reference[index] = 100;
-//         } else {
-//             counter_reference[index] += 1;
 //         }
-//         break;
+//     }
 
-//     default:
-//         break;
+//     if (GPIO_ReadInputPin(CTRL_PORT, MINUS) == RESET)
+//     {
+//         counter_reference[index] -= 1;
+//         if (counter_reference[index] <= 0)
+//         {
+//             counter_reference[index] = 0;
+//         }
 //     }
 // }
+
+void colour_control(void)
+{
+    if (GPIO_ReadInputPin(CTRL_PORT, CHANGE) == RESET)
+    {
+        index += 1;
+        if (index > 3)
+        {
+            index = 1;
+        }
+        return;
+    }
+
+    if (GPIO_ReadInputPin(CTRL_PORT, PLUS) == RESET)
+    {
+        counter_reference[index] += 1;
+        if (counter_reference[index] >= 100)
+        {
+            counter_reference[index] = 100;
+        }
+        return;
+    }
+
+    if (GPIO_ReadInputPin(CTRL_PORT, MINUS) == RESET)
+    {
+        counter_reference[index] -= 1;
+        if (counter_reference[index] <= 0)
+        {
+            counter_reference[index] = 0;
+        }
+        return;
+    }
+}
 
 void counter_reset(void)
 {
@@ -83,7 +111,7 @@ int main(void)
     GPIO_Init(CTRL_PORT, CHANGE,  GPIO_MODE_IN_PU_NO_IT);
     GPIO_Init(CTRL_PORT, PLUS,    GPIO_MODE_IN_PU_NO_IT);
 
-    counter_reset();
+    counter_reset();  
 
     while (1)
     {
@@ -91,81 +119,50 @@ int main(void)
         if (micros() - master_time >= master_period)
         {
 
-           if (counter[1] > 0)
-           {
+            if (counter[1] > 0)
+            {
                 ON(RGB_PORT, RED_PIN);
                 counter[1] -= 1;
 
-           } else {
+            } else {
                 if (counter[1] == 0)
                 {
                     OFF(RGB_PORT, RED_PIN);
                 }
-           }
+            }
 
-           if (counter[2] > 0)
-           {
+            if (counter[2] > 0)
+            {
                 ON(RGB_PORT, GRN_PIN);
                 counter[2] -= 1;
 
-           } else {
+            } else {
                 if (counter[2] == 0)
                 {
                     OFF(RGB_PORT, GRN_PIN);
                 }
-           }
+            }
 
-           if (counter[3] > 0)
-           {
+            if (counter[3] > 0)
+            {
                 ON(RGB_PORT, BLU_PIN);
                 counter[3] -= 1;
 
-           } else {
+            } else {
                 if (counter[3] == 0)
                 {
                     OFF(RGB_PORT, BLU_PIN);
                 }
-           }
+            }
 
            
             counter[0] += 1;
             if (counter[0] > 100)
             {
+                colour_control();
                 counter_reset();
             }      
             master_time = micros();
-
-
-            switch (GPIO_ReadInputData(CTRL_PORT))
-            {
-                case 0b010:
-                    if ((index + 1) > 2)
-                    {
-                        index = 0;
-                    } else {
-                        index += 1;
-                    }
-                    break;    
-
-                case 0b001:
-                    if ((counter_reference[index] - 1) < 0)
-                    {
-                        counter_reference[index] = 0;
-                    } else {
-                        counter_reference[index] -= 1;
-                    }
-                    break;
-
-                case 0b100:
-                    if ((counter_reference[index] + 1) > 100)
-                    {
-                        counter_reference[index] = 100;
-                    } else {
-                        counter_reference[index] += 1;
-                    }
-                    break;
-                    
-            }
         }
     }
 }
